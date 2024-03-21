c8f114c747c1e0d288a00cd93c566a31ad941ea6
hfcai <haifei.cai@gientech.com>
2024-03-21 10:03:44 +0800

    update

---
+++ server/src/controller/template.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Inject } from '@midwayjs/core';
+import { Context } from '@midwayjs/koa';
+import { ApiTags } from '@midwayjs/swagger';
+
+@ApiTags(['role'])
+@Controller('/api/role')
+export class TemplateController {
+  @Inject()
+  ctx: Context;
+}
