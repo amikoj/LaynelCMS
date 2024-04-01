8825d39351f35e704f4a68ea223ebf8e0410319b
hfcai <haifei.cai@gientech.com>
2024-04-01 09:26:29 +0800

    feat: 创建后端主要路由地址

--- server/src/controller/admin.controller.ts
+++ server/src/controller/admin.controller.ts
@@ -12,4 +12,69 @@ export class AdminController {
   async login() {
     await this.ctx.render('admin/login');
   }
+
+  @Get('/')
+  @Get('/dashborad')
+  async dashborad() {
+    await this.ctx.render('admin/dashborad');
+  }
+
+  /**
+   * 用户管理
+   */
+  @Get('/system')
+  @Get('/system/users')
+  async userManager() {
+    await this.ctx.render('admin/system/users');
+  }
+
+  @Get('/system/roles')
+  async roleManager() {
+    await this.ctx.render('admin/system/user');
+  }
+
+  /**
+   * 插件管理
+   */
+  @Get('/plugin')
+  @Get('/plugin/index')
+  async plugin() {
+    await this.ctx.render('admin/plugin/index');
+  }
+
+  /**
+   * 内容管理
+   */
+  @Get('/article')
+  @Get('/article/index')
+  async article() {
+    await this.ctx.render('admin/article/index');
+  }
+
+  /**
+   * web端菜单管理
+   */
+  @Get('/appearance')
+  @Get('/appearance/menu')
+  async menu() {
+    await this.ctx.render('admin/appearance/menu');
+  }
+
+  /**
+   * 小工具管理
+   */
+  @Get('/appearance')
+  @Get('/appearance/tool')
+  async tool() {
+    await this.ctx.render('admin/appearance/tool');
+  }
+
+  /**
+   * 设置
+   */
+  @Get('/settings')
+  @Get('/settings/normal')
+  async normalSettings() {
+    await this.ctx.render('admin/settings/normal');
+  }
 }
