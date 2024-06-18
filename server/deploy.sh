c7c71301d5e3a9b682bfbfa7c975a99aa4de4881
胖菜 <hfcaiDev@gmail.com>
2024-06-18 23:13:17 +0800

    update

---
+++ server/deploy.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+docker rmi laynel-api:latest
+docker rmi registry.cn-shanghai.aliyuncs.com/enjoytoday/laynel-api:latest
+docker build -t laynel-api:latest .
+docker tag laynel-api:latest registry.cn-shanghai.aliyuncs.com/enjoytoday/laynel-api:latest
+docker push registry.cn-shanghai.aliyuncs.com/enjoytoday/laynel-api:latest
