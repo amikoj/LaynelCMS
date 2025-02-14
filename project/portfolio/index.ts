b186c99a90a3ce92bd8a2965d05af7a952169125
hfcai <haifei.cai@gientech.com>
2025-02-14 11:03:32 +0800

    feat: 添加基础路由配置

---
+++ project/portfolio/index.ts
@@ -0,0 +1,7 @@
+import { withInstall } from "@laynel-ui/utils";
+import Dashborad from "./page.vue";
+
+// 导出一个page组件
+const Page = withInstall(Dashborad); 
+
+export default Page; //导出组件
---
+++ project/subject/page.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const a = ref('hello world')
+
+</script>
+
+<template>
+    <div>
+         This is the page component
+        <h1>{{a}}</h1>
+    </div>
+</template>
